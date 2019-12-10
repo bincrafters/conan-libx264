27d8fe4148788d4fd07a16a2e6405a949a4d4f31
Croydon <git@cr0ydon.com>
2019-12-10 18:16:48 +0100

    Remove requires from build.py

--- build.py
+++ build.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-
-from bincrafters import build_template_default, build_shared
-import platform
-import os
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder(pure_c=True)
-
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        installers = ["nasm_installer/2.13.02@bincrafters/stable"]
-        if build_shared.get_os() == "Windows":
-            installers.append("msys2/20161025")
-        build_requires.update({"*": installers})
 
     builder.run()
